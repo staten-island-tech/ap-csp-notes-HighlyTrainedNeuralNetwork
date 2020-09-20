413eb4415a7e215b83da61c424288ab2528ad03b
Andrei Klepatch <56235713+HighlyTrainedNeuralNetwork@users.noreply.github.com>
2020-09-20 19:01:38 -0400

    Cycle 0 Assignment

---
+++ Cycle_0_Notes.js
@@ -0,0 +1,9 @@
+// variables are symbolic names used to store and modify values
+// variable can be created with the syntax var name = some value ;
+// ';' is neccesary at the end of a line because JS uses it to separate lines
+// use '' around strings
+// '//' can be used to create single line comments
+// three primary types of variables are integers, strings, and booleans
+// other variable types include undefined and null
+// the value stored in a constant cannot be modified
+// variables have a scope that determines where in the code they can be accessed
